8c7e40281aa9f4e59d95a7eb3372786d055a0903
Titas Ghosh <rg814602@gmail.com>
2024-07-22 21:21:26 +0530

    add <sup/>

--- src/components/section/WorkSection.jsx
+++ src/components/section/WorkSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef } from "react";
 import HorizantalScroll from "./HorizantalScroll";
+import {work} from '@/data/works'
 const WorkSection = () => {
   const container = useRef(null);
   return (
@@ -8,7 +9,7 @@ const WorkSection = () => {
       ref={container}
       className="redd flex h-[200vh] w-screen flex-col items-center justify-start"
     >
-      <h1 className="p-2 text-5xl">PROJECTS</h1>
+      <h1 className="p-2 text-5xl">PROJECTS <sup>( {work.length} )</sup> </h1>
       <HorizantalScroll Ref_Container={container} />
     </div>
   );
