f8281abb22f7b2e7886c899f6ddb8ef63796258c
Titas Ghosh <rg814602@gmail.com>
2024-07-24 19:44:06 +0530

    -r

--- src/components/section/HorizantalScroll.jsx
+++ src/components/section/HorizantalScroll.jsx
@@ -13,9 +13,9 @@ const HorizantalScroll = ({ Ref_Container }) => {
   // const xmobile = useTransform(scrollYProgress, [0, 1], ["42%", "-42%"]);
 
   return (
-    <div className="green sticky top-20 flex h-screen w-full items-center justify-center overflow-hidden px-32">
+    <div className="greenn sticky top-20 flex h-screen w-full items-center justify-center overflow-hidden px-32">
       <motion.div
-        className="red flex h-screen min-w-[280rem] items-center justify-end gap-5 p-10 laptop:min-w-[260rem] desktop:min-w-[200rem]"
+        className="redd flex h-screen min-w-[280rem] items-center justify-end gap-5 p-10 laptop:min-w-[260rem] desktop:min-w-[200rem]"
         style={{ x }}
       >
         {work.map((data, i) => {
