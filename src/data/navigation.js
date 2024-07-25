63c01261ba751000e5a9fe604d1c7655dfb0f5b6
Titas Ghosh <rg814602@gmail.com>
2024-07-25 08:44:52 +0530

    -c work to demos

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,5 +1,5 @@
 export const navigation = [
   { id: 1, route: "/about", lable: "About Us " },
-  { id: 2, route: "/work", lable: "Work" },
+  { id: 2, route: "/work", lable: "Demos" },
   { id: 3, route: "/contact", lable: "Contact" },
 ];
