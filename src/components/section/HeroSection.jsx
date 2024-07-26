0cbd021a64f34067d590abd4a51fbe5c5942ce26
Titas Ghosh <rg814602@gmail.com>
2024-07-26 08:14:22 +0530

    change hero sections color and slogan

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -25,19 +25,19 @@ const HeroSection = () => {
     <div className="redd flex min-h-screen w-screen flex-col items-center justify-center">
       <div className="redd flex h-full w-full items-end justify-center">
         <h1
-          className="split-text-yourmaker flex-center redd w-full  h-fit overflow-hidden text-[3.5rem]  font-extrabold tablet:text-8xl laptop:text-9xl desktop:text-[10rem]"
+          className="split-text-yourmaker flex-center redd h-fit w-full overflow-hidden text-[3.5rem] font-bold text-orange-700 tablet:text-8xl laptop:text-9xl desktop:text-[10rem]"
           // style={{ transform: "translateY(120px)" }}
         >
-          YOURMAKER
+          YOUR MAKER
         </h1>
       </div>
-      <div className="redd flex h-full w-full items-center justify-end pr-10">
-        <div className="flex items-end redd laptop:text-3xl desktop:text-4xl h-full w-fit  tablet:text-2xl flex-col gap-2 p-5 text-xl">
-          <h1 className="split-text-line  redd desktop:w-[50rem] flex items-center justify-center w-full overflow-hidden">
-            Bridging the gap between now and next with
+      <div className="redd flex h-96 w-full items-center justify-end pr-10">
+        <div className="redd flex h-full w-fit flex-col items-center gap-2 p-5 text-xl tablet:text-2xl laptop:text-3xl desktop:text-4xl">
+          <h1 className="split-text-line redd flex w-full items-center justify-start overflow-hidden desktop:w-[50rem]">
+            Wanna Grow Your Bussiness
           </h1>
-          <h1 className="split-text-line  redd w-full overflow-hidden">
-            strategic innovation in design and technology
+          <h1 className="split-text-line redd flex w-full items-end justify-center overflow-hidden">
+            We Are Here For You
           </h1>
         </div>
       </div>
