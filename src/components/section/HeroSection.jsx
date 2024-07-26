08cc945a67372220035e70fb20e383aa41d2c99f
Titas Ghosh <rg814602@gmail.com>
2024-07-26 08:14:42 +0530

    color

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
     <div className="redd flex min-h-screen w-screen flex-col items-center justify-center">
       <div className="redd flex h-full w-full items-end justify-center">
         <h1
-          className="split-text-yourmaker flex-center redd h-fit w-full overflow-hidden text-[3.5rem] font-bold text-orange-700 tablet:text-8xl laptop:text-9xl desktop:text-[10rem]"
+          className="split-text-yourmaker flex-center redd h-fit w-full overflow-hidden text-[3.5rem] font-bold text-orange-600 tablet:text-8xl laptop:text-9xl desktop:text-[10rem]"
           // style={{ transform: "translateY(120px)" }}
         >
           YOUR MAKER
