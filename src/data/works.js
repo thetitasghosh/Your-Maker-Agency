d955bac1379773bcbe0a47f591287765ca80fef5
Titas Ghosh <rg814602@gmail.com>
2024-07-26 20:16:06 +0530

    add 2 new Poster

--- src/data/works.js
+++ src/data/works.js
@@ -3,20 +3,32 @@ import FDP from "../../public/img/projects/Food_Demo_Poster_Your_Maker.webp";
 import GDP from "../../public/img/projects/Galaxy_Demo_Poster_Design_Your_Maker.webp";
 import S23SDP from "../../public/img/projects/S23_Series_Demo_Poster_Your_Maker.webp";
 import W7DP from "../../public/img/projects/Watch_7_Demo_Poster_Your_Maker.webp";
+import F6GBD from "../../public/img/projects/Flip 6 Glass Broken Demo Poster - Your Maker.webp";
+import F6RD from "../../public/img/projects/Fold 6 Ribbon Demo Poster - Your Maker.webp";
 
 export const work = [
   {
     id: 1,
+    img: F6GBD,
+    info: "Flip 6 Glass Broken Demo Poster",
+  },
+  {
+    id: 2,
+    img: F6RD,
+    info: "Fold 6 Ribbon Demo Poste",
+  },
+  {
+    id: 3,
     img: ADP,
     info: "Accessories Demo Poster",
   },
-  { id: 2, img: FDP, info: "Food Demo Poster" },
-  { id: 3, img: GDP, info: "Galaxy Demo Poster Design" },
-  { id: 4, img: S23SDP, info: "S23 Series Demo Poster" },
-  { id: 5, img: W7DP, info: "Watch 7 Demo Poster" },
-  { id: 6, img: FDP, info: "Food Demo Poster" },
-  { id: 7, img: ADP, info: "Accessories Demo Poster" },
+  { id: 4, img: GDP, info: "Galaxy Demo Poster Design" },
+  { id: 5, img: S23SDP, info: "S23 Series Demo Poster" },
+  { id: 6, img: W7DP, info: "Watch 7 Demo Poster" },
+  { id: 7, img: FDP, info: "Food Demo Poster" },
+  // { id: 6, img: FDP, info: "Food Demo Poster" },
+  // { id: 7, img: ADP, info: "Accessories Demo Poster" },
   //   { id: 8, text: "FIFTH" },inDif:"1",
 ];
 
-export const InDefer = [{ i: 0.4 }, { i: 1 }, { i: 0.3 }, { i: 0.8 }];
+// export const InDefer = [{ i: 0.4 }, { i: 1 }, { i: 0.3 }, { i: 0.8 }];
