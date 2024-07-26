87a300e93c8043f8a70b4d1fd8fb7fa3a13db5f1
Titas Ghosh <rg814602@gmail.com>
2024-07-26 21:00:19 +0530

    -c

--- src/app/demo/page.jsx
+++ src/app/demo/page.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import WorkList from "@/components/ui/WorkList";
+import WorkSection from "@/components/section/WorkSection";
 export const metadata = {
   title: "Works",
   description: "...",
 };
 const WorkPage = () => {
   return (
-    <div className="flex-sc min-h-screen gap-5 p-10">
-      <WorkList />
+    <div className="w-full flex min-h-screen flex-col ">
+      <h1 className="redd mt-20 text-3xl pl-5 tablet:text-5xl desktop:ml-20 desktop:text-5xl">
+        Selected Demos
+      </h1>
+      <WorkSection />
     </div>
   );
 };
