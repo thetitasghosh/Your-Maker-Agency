5f5d8042694694e813a6fb0fceab313428d4474f
Titas Ghosh <rg814602@gmail.com>
2024-07-28 21:52:29 +0530

    add service route

---
+++ src/app/service/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+        <h1 className='text-9xl'>Service Page</h1>
+    </div>
+  );
+}
+
+export default page;
