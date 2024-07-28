1e535749878b97abbc4faf1e7bf75ec34c0c267c
Titas Ghosh <rg814602@gmail.com>
2024-07-28 22:00:22 +0530

    -c

--- src/components/section/ServiceSection.jsx
+++ src/components/section/ServiceSection.jsx
@@ -2,17 +2,22 @@ import React from "react";
 
 const ServiceSection = () => {
   return (
-    <div className="red flex h-screen w-screen items-center justify-center">
-      <div className="flex h-full w-full flex-col items-center justify-center">
-        <div className="red flex h-full w-full items-center justify-center">
-          <div className="red h-full w-96">text</div>
+    <div className="redd flex h-screen w-screen items-center justify-center">
+      <div className="flex h-full w-full flex-col gap-5 items-center justify-center">
+        <h1 className="redd flex w-full items-center justify-center py-10 text-5xl uppercase">
+          Our Service
+        </h1>
+        <div className="redd flex h-full w-full items-center justify-center">
+          <div className="redd flex h-full w-96 items-center justify-center">
+            text
+          </div>
           <div className="flex h-full w-full items-center justify-center gap-5">
             {["Product Photography", "Local SEO", "Social Media Marketing"].map(
               (data, idx) => {
                 return (
                   <div
                     key={idx}
-                    className="red text-yourMaker flex size-96 items-center justify-center rounded-2xl"
+                    className="flex size-96 items-center justify-center rounded-2xl border border-yourMaker text-yourMaker"
                   >
                     {data}
                   </div>
@@ -21,14 +26,16 @@ const ServiceSection = () => {
             )}
           </div>
         </div>
-        <div className="red flex h-full w-full flex-row-reverse">
-          <div className="red h-full w-96">text</div>
+        <div className="redd flex h-full w-full flex-row-reverse">
+          <div className="redd flex h-full w-96 items-center justify-center">
+            text
+          </div>
           <div className="flex h-full w-full items-center justify-center gap-5">
             {["Website D&D", "Graphic Design", "SEO"].map((data, idx) => {
               return (
                 <div
                   key={idx}
-                  className="red text-yourMaker flex size-96 items-center justify-center rounded-2xl"
+                  className="flex size-96 items-center justify-center rounded-2xl border border-yourMaker text-yourMaker"
                 >
                   {data}
                 </div>
