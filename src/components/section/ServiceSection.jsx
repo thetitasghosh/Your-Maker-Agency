bf15f6db684e17b4ed5e34988b183ab276bd4926
Titas Ghosh <rg814602@gmail.com>
2024-08-02 08:22:48 +0530

    responsive service box

--- src/components/section/ServiceSection.jsx
+++ src/components/section/ServiceSection.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const ServiceSection = () => {
   return (
-    <div className="redd flex h-screen w-screen items-center justify-center">
-      <div className="flex h-full w-full flex-col gap-5 items-center justify-center">
+    <div className="redd flex min-h-screen w-screen items-center justify-center">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-5">
         <h1 className="redd flex w-full items-center justify-center py-10 text-5xl uppercase">
           Our Service
         </h1>
-        <div className="redd flex h-full w-full items-center justify-center">
+        <div className="redd flex h-full w-full flex-col items-center justify-center desktop:flex-row">
           <div className="redd flex h-full w-96 items-center justify-center">
             text
           </div>
-          <div className="flex h-full w-full items-center justify-center gap-5">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-5 desktop:flex-row">
             {["Product Photography", "Local SEO", "Social Media Marketing"].map(
               (data, idx) => {
                 return (
@@ -26,11 +26,11 @@ const ServiceSection = () => {
             )}
           </div>
         </div>
-        <div className="redd flex h-full w-full flex-row-reverse">
+        <div className="redd desktop:flex-row-reverseh-full flex w-full flex-col">
           <div className="redd flex h-full w-96 items-center justify-center">
             text
           </div>
-          <div className="flex h-full w-full items-center justify-center gap-5">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-5 desktop:flex-row">
             {["Website D&D", "Graphic Design", "SEO"].map((data, idx) => {
               return (
                 <div
