c095708f4c1b8a0c63192d1f6afd18f4966a423c
Titas Ghosh <rg814602@gmail.com>
2024-08-04 19:43:39 +0530

    add new hero section

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -1,92 +1,91 @@
 "use client";
 import gsap from "gsap";
-import React, { useLayoutEffect } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import React, { useLayoutEffect, useState } from "react";
 import SplitType from "split-type";
+import { cn } from "@/libs/cn";
 
 const HeroSection = () => {
-  useLayoutEffect(() => {
-    const YM_title = new SplitType(".split-text-yourmaker", {
-      types: "words",
-    });
-    const YM_line = new SplitType(".split-text-line", { types: "lines" });
-    const ctx = gsap.context(() => {
-      let tl = gsap.timeline({
-        delay: 1.5,
-      });
-      tl.from(
-        YM_title.words[0],
-        {
-          y: "200%",
-          // scale: 0.5,
-          duration: 0.5,
-        },
-        "1",
-      );
-      tl.from(
-        YM_title.words[1],
-        {
-          y: "-200%",
-          // scale: 0.5,
-          duration: 0.5,
-        },
-        "1",
-      );
-      tl.from(
-        YM_title.words[0],
-        {
-          scale: 1.5,
-        },
-        "2",
-      );
-      tl.from(
-        YM_title.words[1],
-        {
-          scale: 1.5,
-        },
-        "2",
-      );
-      tl.from(YM_line.lines, { y: "200%", stagger: { amount: 0.1 } });
-      tl.from(".Btn", {
-        opacity: 0,
-        stagger: { amount: 0.5 },
-      });
-      tl.from(".Qs", {
-        y: "150%",
-      });
-    });
-    return () => ctx.revert();
-  }, []);
+  const [hover, setHover] = useState(false);
+
+  // useLayoutEffect(() => {
+  //   const title = new SplitType(".split-text-yourmaker", {
+  //     types: "lines",
+  //   });
+  //   const line = new SplitType(".split-text-sum", { types: "lines" });
+  //   const ctx = gsap.context(() => {
+  //     let tl = gsap.timeline({
+  //       delay: 2,
+  //     });
+  //     tl.from(
+  //       title.lines[0],
+  //       {
+  //         opacity: 0,
+  //         y: "50%",
+  //         filter: "blur(10px)",
+  //       },
+  //       "1",
+  //     );
+  //     tl.from(
+  //       title.lines[1],
+  //       {
+  //         opacity: 0,
+  //         y: "50%",
+  //         filter: "blur(10px)",
+  //       },
+  //       "1",
+  //     );
+  //     tl.from(line.lines, {
+  //       opacity: 0,
+  //       y: "50%",
+  //       filter: "blur(10px)",
+  //     });
+  //     tl.from(".Btn1", {
+  //       opacity: 0,
+  //       y: "50%",
+  //       filter: "blur(10px)",
+  //     });
+  //     tl.from(".Btn2", {
+  //       opacity: 0,
+  //       y: "50%",
+  //       filter: "blur(10px)",
+  //     });
+  //   });
+  //   return () => ctx.revert();
+  // }, []);
   return (
-    <div className="redd flex min-h-screen w-screen flex-col items-center justify-center">
-      <div className="redd flex h-full w-full items-end justify-center">
-        <p
-          className="split-text-yourmaker redd flex h-fit w-full items-center justify-center overflow-hidden text-[3.5rem] font-bold text-orange-600 tablet:text-8xl laptop:text-9xl desktop:text-[10rem]"
-          // style={{ transform: "translateY(120px)" }}
-        >
-          YOUR MAKER
-        </p>
-      </div>
-      <div className="redd flex h-96 w-full flex-col items-start justify-center">
-        <div className="redd flex h-fit w-full flex-col items-center justify-center gap-2 p-5 text-2xl tablet:text-2xl laptop:text-3xl desktop:text-5xl">
-          <h1 className="split-text-line redd flex w-fit items-center justify-center overflow-hidden pl-8 tablet:pl-20  tablet:px-10">
-            Wanna Grow Your Bussiness
-            <span className="Qs redd block h-fit w-8 select-none overflow-hidden">
-              ?
-            </span>
+    <div className="redd flex min-h-screen w-screen items-center justify-evenly">
+      <div className="redd flex h-full w-[50rem] flex-col items-center justify-center gap-12">
+        <div className="redd flex h-fit w-full flex-col items-start justify-center gap-2 text-8xl uppercase">
+          <h1 className="split-text-yourmaker font-bold text-yourMaker">
+            wanna grow
           </h1>
-          <h1 className="split-text-line redd flex w-fit items-end justify-center overflow-hidden">
-            We Are Here For You
-          </h1>
+          <h1 className="split-text-yourmaker font-light">your business</h1>
         </div>
-        <div className="redd flex h-full w-full items-center justify-center gap-20 font-semibold">
-          <button className="Btn rounded-2xl bg-yourMaker p-2 px-5">
+        <div className="w-full capitalize">
+          <h1 className="split-text-sum text-5xl">we are here for you</h1>
+        </div>
+        <div className="reddd flex w-full items-center justify-start gap-5">
+          <button className="Btn1 rounded-xl bg-yourMaker p-1 px-3 font-semibold text-white">
             See Plans
           </button>
-          <button className="Btn rounded-2xl border border-yourMaker p-2 px-5 text-yourMaker">
+          <button
+            onMouseEnter={() => setHover(!hover)}
+            onMouseLeave={() => setHover(!hover)}
+            className="Btn2 flex items-center justify-center gap-2 rounded-xl border border-yourMaker p-1 px-3 text-yourMaker"
+          >
             Contact
+            <FaArrowRight
+              className={cn("transition-all opacity-5 duration-500", {
+                // block: hover,
+                "opacity-100": hover,
+                "-rotate-45": hover,
+              })}
+            />
           </button>
         </div>
       </div>
+      <div className="redd h-96 w-96"></div>
     </div>
   );
 };
