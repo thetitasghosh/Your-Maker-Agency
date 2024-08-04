359985a210f1394e49d2ba35c958753a712b45a2
Titas Ghosh <rg814602@gmail.com>
2024-08-04 19:54:35 +0530

    fix layout

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -5,15 +5,17 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="redd flex h-full w-full items-center justify-center">
-      <Link href={"/"} className="flex items-center justify-center">
-        <Image
-          className="redd size-9 select-none text-white"
-          src={"/assets/YM.svg"}
-          width={100}
-          height={100}
-          alt="Your-Maker Log0"
-          priority
-        />
+      <Link href={"/"} className="flex h-full items-center justify-center">
+        <div className="redd size-10 mt-2 flex items-center justify-center">
+          <Image
+            className="redd select-none text-white"
+            src={"/assets/YM.svg"}
+            width={100}
+            height={100}
+            alt="Your-Maker Log0"
+            priority
+          />
+        </div>
         <h1 className="hidden text-4xl font-semibold laptop:block">
           <span className="text-orange-600">YOUR</span> MAKER
         </h1>
