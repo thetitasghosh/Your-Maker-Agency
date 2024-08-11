7a187a77edd7c64a6e3c30798197fb10fae65e67
Titas Ghosh <rg814602@gmail.com>
2024-08-11 11:49:32 +0530

    add demo cards

--- src/components/section/GridLayout.jsx
+++ src/components/section/GridLayout.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import DemoCard from '../ui/Card'
+import React from "react";
+
+import { work } from "@/data/works";
+import Image from "next/image";
+import { cn } from "@/libs/cn";
 const GridLayout = () => {
   return (
-    <div className='min-h-screen w-screen flex flex-wrap items-center justify-center gap-5'>
-      
+    <div className="redd flex min-h-screen w-screen flex-wrap items-center justify-center gap-5 p-10">
+      <div className="btns w-full red h-20"></div>
+      <div className="w-full h-full flex flex-wrap items-center justify-center gap-10">
+        {work.map((data, i) => {
+          return <WorkCard key={i} data={data} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+function WorkCard({ data }) {
+  return (
+    <div
+      style={{ backgroundColor: data.color }}
+      className={cn(
+        `redd relative flex h-full w-[30rem] items-center justify-center bg-white p-10 text-4xl`,
+      )}
+    >
+      <div className="redd relative flex h-96 w-96 cursor-pointer flex-col items-center justify-center gap-1 rounded-md">
+        <div className="redd flex w-full items-center justify-center shadow-2xl">
+          <Image
+            src={data.img}
+            width={1000}
+            height={500}
+            className="redd w-full"
+            alt="Your Maker Demo  Poster"
+          />
+        </div>
+      </div>
     </div>
   );
 }
