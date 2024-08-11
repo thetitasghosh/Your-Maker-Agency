ab8486c14e26477b742fa13bcd5e7f12cecd0ede
Titas Ghosh <rg814602@gmail.com>
2024-08-11 11:58:30 +0530

    add filter on project silce8

--- src/components/section/HorizantalScroll.jsx
+++ src/components/section/HorizantalScroll.jsx
@@ -12,13 +12,14 @@ const HorizantalScroll = ({ Ref_Container }) => {
   const x = useTransform(scrollYProgress, [0, 1], ["23%", "-23%"]);
   // const xmobile = useTransform(scrollYProgress, [0, 1], ["42%", "-42%"]);
 
+  const FilterDemos = work.slice(0, 7);
   return (
     <div className="greenn sticky top-8 flex h-screen w-full items-center justify-center overflow-hidden px-32">
       <motion.div
         className="redd flex h-screen min-w-[425rem] items-center justify-center gap-10 p-10 tablet:min-w-[400rem] laptop:min-w-[410rem] desktop:min-w-[200rem]"
         style={{ x }}
       >
-        {work.map((data, i) => {
+        {FilterDemos.map((data, i) => {
           return <Card key={i} data={data} />;
         })}
       </motion.div>
