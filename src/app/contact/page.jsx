f2ed989f810a581269765d2c054b3108219140f6
Titas Ghosh <rg814602@gmail.com>
2024-09-01 19:33:14 +0530

    add contact page contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ContactLayout from "@/components/section/ContactLayout";
+import { Social } from "@/data/navigation";
 export const metadata = {
   title: "Contact Us",
   description: "...",
@@ -6,7 +8,32 @@ export const metadata = {
 const ContactPage = () => {
   return (
     <div className="flex-sc">
-      <h1 className="text-5xl">Contact Page</h1>
+      <div className="redd flex h-full w-full flex-col items-center justify-center gap-10 pl-20">
+        <div className="redd w-full">
+          <p className="text-5xl font-semibold">
+            Let&apos;s unlock together the next <br /> level of possibilities!
+            Reach out.
+          </p>
+        </div>
+        <div className="redd flex w-full flex-col">
+          <h1 className="text-3xl font-medium uppercase text-stone-700">
+            social media
+          </h1>
+          <div className="flex w-full items-center justify-start gap-2">
+            {Social.map((data, i) => {
+              return (
+                <div className="text-2xl" key={i}>
+                  {data.social}{" "}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className="h-full flex  w-full">
+        <ContactLayout />
+      </div>
     </div>
   );
 };
---
+++ src/components/section/ContactLayout.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ArrowUpRight } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "../ui/button";
+const ContactLayout = () => {
+  return (
+    <div className="flex h-full w-full items-end justify-center border border-orange-500">
+      <form
+        action=""
+        className="redd mb-5 flex flex-col items-center justify-between px-5 desktop:h-[45rem] desktop:w-[50rem]"
+      >
+        <div className="gnn flex items-start justify-between  h-96 w-full  flex-col">
+          <Label htmlFor="name" className="pl-2 text-2xl">
+            Name
+          </Label>
+          <Input id="name"></Input>
+          <Label htmlFor="email" className="pl-2 text-2xl">
+            Email
+          </Label>
+          <Input id="email"></Input>
+          <Label htmlFor="company" className="pl-2 text-2xl">
+            Company
+          </Label>
+          <Input id="company"></Input>
+          <div className="redd my-2 w-full h-14 flex items-center justify-center">
+            <Select>
+              <SelectTrigger className="w-full h-full text-2xl text-yourMaker">
+                <SelectValue placeholder="Select a service you want" />
+              </SelectTrigger>
+              <SelectContent className="bg-black  text-white">
+                {/* <SelectGroup> */}
+                {/* <SelectLabel>Fruits</SelectLabel> */}
+                <SelectItem value="digital">Digital Marketing</SelectItem>
+                <SelectItem value="product">Product Photography</SelectItem>
+                <SelectItem value="graphics">Graphic Design</SelectItem>
+                <SelectItem value="web">
+                  Website Design & Development
+                </SelectItem>
+                {/* <SelectItem value="pineapple">Pineapple</SelectItem> */}
+                {/* </SelectGroup> */}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <Textarea placeholder="Type your message here." />
+        <Button
+          type="submit"
+          size={"lg"}
+          variant={"secondary"}
+          className="w-full text-2xl"
+        >
+          Send <ArrowUpRight />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactLayout;
