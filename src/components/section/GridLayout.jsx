10aacaac98763b80c5baa0020feaa9f9a8b045ee
Titas Ghosh <rg814602@gmail.com>
2024-09-01 19:50:00 +0530

    cn

--- src/components/section/GridLayout.jsx
+++ src/components/section/GridLayout.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
 import { work } from "@/data/works";
 import Image from "next/image";
-import { cn } from "@/libs/cn";
+import { cn } from "@/lib/utils";
 const GridLayout = () => {
   return (
     <div className="redd flex min-h-screen w-screen flex-wrap items-center justify-center gap-5 p-10">
-      <div className="btns w-full red h-20"></div>
-      <div className="w-full h-full flex flex-wrap items-center justify-center gap-10">
+      <div className="btns red h-20 w-full"></div>
+      <div className="flex h-full w-full flex-wrap items-center justify-center gap-10">
         {work.map((data, i) => {
           return <WorkCard key={i} data={data} />;
         })}
@@ -21,7 +20,7 @@ function WorkCard({ data }) {
     <div
       // style={{ backgroundColor: data.color }}
       className={cn(
-        `redd relative flex h-full w-[30rem] items-center justify-center  p-10 text-4xl`,
+        `redd relative flex h-full w-[30rem] items-center justify-center p-10 text-4xl`,
       )}
     >
       <div className="redd relative flex h-96 w-96 cursor-pointer flex-col items-center justify-center gap-1 rounded-md">
